8b50675f960cc6d5f681b28815ee0a70e759a4eb
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2022-10-25 17:48:19 +0700

    added summaryt

--- src/AuthServices.js
+++ src/AuthServices.js
@@ -17,10 +17,29 @@ export const GetLogin = async (info) => {
         .then(response => response.json())
         .then(result => { return result })
         .catch(error => console.log('error', error));
-        // console.log(res);
+    // console.log(res);
     if (res.user != null && res.user.role == 'Student') {
         res = { token: '', error: 'Invalid username or password!', user: null, status: 'FAILED' }
     }
 
     return res;
+}
+export const GetSummary = async (info) => {
+    var formdata = new FormData();
+    formdata.append("api_token", info.api_token);
+    formdata.append("user_token", info.user_token);
+
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    const res = await fetch(process.env.REACT_APP_API_SUMMARY, requestOptions)
+        .then(response => response.json())
+        .then(result => { return result })
+        .catch(error => console.log('error', error));
+        // console.log(res);
+    return res;
+
 }
--- src/Teacher/TeacherDash.js
+++ src/Teacher/TeacherDash.js
@@ -1,7 +1,91 @@
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
+import { GetSummary } from '../AuthServices';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '../Teacher/Teacher.css'
 
 export default function TeacherDash() {
+
+  const teacher = JSON.parse(localStorage.getItem('user'));
+  const [summary, setSummary] = useState({});
+  const [groups, setGroups] = useState(0);
+  const [books, setBooks] = useState(0);
+
+  //here we use useEffect to get Non-promise data the set what need
+  useEffect(() => {
+    GetSummary({ api_token: process.env.REACT_APP_API_TOKEN, user_token: teacher.token })
+      .then(data => {
+        if (data.status === 'SUCCESS') {
+          setGroups(data.summary.groups);
+          setBooks(data.summary.books);
+        } else {
+          setGroups('Error cannot find data');
+          setBooks('Error cannot find data');
+        }
+      })
+  }, [(process.env.REACT_APP_API_TOKEN), (teacher.token)]);
+
+
+  // console.log(summary);
+  const DisplayCard = (props) => {
+    return (
+      <CardContent >
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {props.title}
+        </Typography>
+        <Typography variant="h5" component="div" sx={{ display: 'flex', justifyContent: 'center', fontSize: 40 }}>
+          {props.count}
+        </Typography>
+        {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography> */}
+      </CardContent>)
+  };
+
   return (
-    <div>TeacherDash</div>
+    <>
+      <div>
+        This need another nav bat just a thin 1
+      </div>
+      <div id="sum-wrapper">
+        <div id="summary-container">
+          <Box id="box" sx={{
+            // minWidth: 275,
+            // maxWidth: 300,
+            border: '1px solid red'
+          }}>
+            <Card variant="outlined">
+              {/*here we using fucntion as JSX to call the element and pass param meter */}
+              <DisplayCard {...{
+                title: 'Number of Groups you in charge:',
+                count: groups,
+              }} />
+            </Card>
+          </Box>
+          <Box id="box" sx={{
+            // minWidth: 275,
+            // maxWidth: 300,
+            border: '1px solid red'
+          }}>
+            <Card variant="outlined">
+              <DisplayCard {...{
+                title: 'Number of Books you posted:',
+                count: books,
+              }} />
+            </Card>
+          </Box>
+        </div>
+      </div>
+
+    </>
   )
 }
